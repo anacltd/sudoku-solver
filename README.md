f5955976599db6e011e769c085b450ce97bb06de
Ana Calatayud <ana.calatayud@predictice.com>
2020-10-17 18:03:08 +0200

    added .py file + updated README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # sudoku-solver
+
+Handy little code to resolve sudokus (yes, even the hard ones)
+
+## How to
+- run `sudoku_solver.py`
+- enter your sudoku grid as a single line (starting from the upper left corner and moving right) with `0` instead of empty boxes. So for example, if your sudoku grid looks like this:
+```python
+- - - - - - - - - - - - - 
+| 9 1   | 3   5 |       |
+|     4 | 1     |   8 5 |
+|   5   | 4   2 |   1   |
+- - - - - - - - - - - - - 
+|     8 |     1 |     4 |
+| 4 2   |       | 1 6   |
+| 1     | 6     | 5     |
+- - - - - - - - - - - - - 
+|       | 2   3 |   7   |
+|   4   |     7 | 6     |
+|       | 9   6 |   5 1 |
+- - - - - - - - - - - - - 
+```
+just enter
+```python
+910305000004100085050402010008001004420000160100600500000203070040007600000906051
+```
+* tada!
---
+++ sudoku_solver.py
@@ -0,0 +1,71 @@
+def line_to_grid(line):
+    subline = [line[i:i + 9] for i in range(0, len(line), 9)]
+    return [[int(i) for i in line] for line in subline]
+
+
+def pprint_grid(grid):
+    print("- "*13)
+    for i in range(len(grid)):
+        if i % 3 ==0 and i != 0:
+            print("- "*13)
+        for j in range(len(grid[0])):
+            if j % 3 == 0 and j != 0:
+                print("| ", end="")
+            elif j == 0:
+                print("| ", end="")
+            print(grid[i][j], end="") if j == 8 else print(str(grid[i][j]) + " ", end="")
+        print(" |")
+    print("- "*13)
+
+
+def solve(grid):
+    found = find_empty(grid)
+    if not found:
+        return True
+    else:
+        row, column = found
+
+    for i in range(1, 10):
+        if is_valid(grid, i, (row, column)):
+            grid[row][column] = i
+            if solve(grid):
+                return True
+            grid[row][column] = 0
+    return False
+
+
+def is_valid(grid, number, position):
+    # check row
+    for i in range(len(grid[0])):
+        if grid[position[0]][i] == number and position[1] != i:
+            return False
+
+    # check column
+    for i in range(len(grid)):
+        if grid[i][position[1]] == number and position[0] != i:
+            return False
+
+    # check square
+    box_x, box_y = position[1] // 3, position[0] // 3
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if grid[i][j] == number and (i, j) != position:
+                return False
+    return True
+
+
+def find_empty(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 0:
+                return i,j
+    return None
+
+
+if __name__ == '__main__':
+    line = input("Enter your sudoku grid as a single line with 0 instead of empty boxes\n")
+    grid = line_to_grid(line)
+    pprint_grid(grid)
+    print("\nSolving...\nDone!\n")
+    solve(grid)
+    pprint_grid(grid)
